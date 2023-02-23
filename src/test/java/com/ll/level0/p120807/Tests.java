d61de1b7837dc54b5a5db100800304be1b041ee5
CatJelly <mej1406@gmail.com>
2023-02-23 10:52:45 +0900

    programmers angle

--- src/test/java/com/ll/level0/p120807/Tests.java
+++ src/test/java/com/ll/level0/p120807/Tests.java
@@ -12,4 +12,15 @@ public class Tests {
         assertThat(new Solution().solution(2, 3)).isEqualTo(-1);
     }
 
+    @Test
+    @DisplayName("11, 11 => 1")
+    void t2() {
+        assertThat(new Solution().solution(11, 11)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("7, 99 => -1")
+    void t3() {
+        assertThat(new Solution().solution(7, 99)).isEqualTo(-1);
+    }
 }
