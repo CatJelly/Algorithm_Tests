f56e1e6f1a79ff3d2809ec7a0f57dc105e17962d
CatJelly <mej1406@gmail.com>
2023-02-23 09:41:29 +0900

    first commit

---
+++ src/main/java/com/ll/level0/p120807/Main.java
@@ -0,0 +1,13 @@
+package com.ll.level0.p120807;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 == num2 ? 1 : -1;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        new Solution().solution(2, 3);
+    }
+}
